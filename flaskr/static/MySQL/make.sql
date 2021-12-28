ea64ae5e98aa2492b83945d58b93d8733af10b82
Joel Kalil <joelkalil1@gmail.com>
2021-12-28 18:56:19 +0100

    Start Git

---
+++ flaskr/static/MySQL/make.sql
@@ -0,0 +1,41 @@
+-- Command to create the database
+CREATE DATABASE `mydb` IF NOT EXISTS;
+
+-- Command to create the Users table
+CREATE TABLE `users`(
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255) NOT NULL,
+    Username VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    Admin BOOLEAN NOT NULL DEFAULT 0,
+
+    PRIMARY KEY(Username)
+);
+
+-- Command to create the Forms table
+CREATE TABLE `forms`(
+    FormName VARCHAR(255) NOT NULL,
+    Creator VARCHAR(255) NOT NULL,
+    Content VARCHAR(21000) NOT NULL,
+    FormID INT NOT NULL AUTO_INCREMENT,
+
+    PRIMARY KEY(FormID)
+);
+
+-- Command to create the Forms table
+CREATE TABLE `templates`(
+    Template VARCHAR(21800),
+    FormID INT NOT NULL,
+
+    PRIMARY KEY(FormID)
+);
+
+
+CREATE TABLE `images`(
+    ImageName VARCHAR(255) NOT NULL,
+    Image VARCHAR(21500) NOT NULL,
+    FormID INT NOT NULL,
+
+    PRIMARY KEY(FormID)
+);
